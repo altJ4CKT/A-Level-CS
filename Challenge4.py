f31e22864262eb5b959ecc4274f924c8a6ef3047
altJ4CKT <jacktolley516@gmail.com>
2024-10-02 12:41:25 +0100

    Convert Kg into pounds (Lbs)

---
+++ Challenge4.py
@@ -0,0 +1,5 @@
+def program():
+    weight_input = float(input("Enter your weight:\n"))
+    weight_in_pounds = weight_input * 2.204
+    print(f"Your weight in pounds is {weight_in_pounds:.2f}")
+program()
