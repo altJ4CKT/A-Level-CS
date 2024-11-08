14b7b3f039e56f5938e3d195fea32f1a843cd617
altJ4CKT <jacktolley516@gmail.com>
2024-11-08 10:12:37 +0000

    Challenge 9

---
+++ Challenge9.py
@@ -0,0 +1,7 @@
+def Challenge9():
+    colour = input("Enter a colour: ").lower()
+    if colour == "red":
+        print("I like red too!")
+    elif colour != "red":
+        print(f"I don't like {colour}, I prefer red.")
+Challenge9()
