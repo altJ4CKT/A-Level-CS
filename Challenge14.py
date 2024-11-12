cfb2275710f4f7312c6df94db5ca4f7ff6098749
altJ4CKT <jacktolley516@gmail.com>
2024-11-12 16:54:33 +0000

    Challenge 14

---
+++ Challenge14.py
@@ -0,0 +1,7 @@
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+              
+answer = number1 // number2
+remainder = number1 % number2
+
+print(f"{number1} divided by {number2} is {answer} with a remainder of {remainder}")
