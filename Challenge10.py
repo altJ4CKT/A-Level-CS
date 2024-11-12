11a8021c08fed79d6529a0161b4b3ed28a1f8725
altJ4CKT <jacktolley516@gmail.com>
2024-11-12 16:52:34 +0000

    Challenge 10

---
+++ Challenge10.py
@@ -0,0 +1,5 @@
+first_name = input("Enter your first name: ")
+surname = input("Enter your surname: ")
+full_name = first_name + " " + surname
+length = len(first_name) + len(surname)
+print(f"Your full name is {full_name}, your name has {length} characters.")
