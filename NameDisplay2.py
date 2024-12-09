5790bdfe5a871a05f779dea761e99c09efbda012
altJ4CKT <jacktolley516@gmail.com>
2024-12-09 14:26:44 +0000

    Challenge18

--- NameDisplay2.py
+++ NameDisplay2.py
@@ -1,5 +1,3 @@
 usr_input = input("Enter your name: ")
-number_input = int(input("Enter a number: "))
-for i in range(number_input):
-    for j in range(len(usr_input)):
-        print(usr_input[j])
+for i in range(len(usr_input)):
+    print(usr_input[i])
